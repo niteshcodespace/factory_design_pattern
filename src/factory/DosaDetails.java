b4d87aa81bc34ddd2e64335ab73eaf5a1a67acb9
Nitesh <nitesh.codespace@gmail.com>
2024-12-06 10:52:07 +0530

    factory design pattern

--- src/factory/DosaDetails.java
+++ src/factory/DosaDetails.java
@@ -5,7 +5,7 @@ public class DosaDetails {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Dosa dosa= DosaFactory.createDosa(DosaTypes.Podi);
-		Dosa dosa= DosaFactory.createDosa(DosaTypes.Ghee);
+		Dosa dosa= DosaFactory.createDosa(DosaTypes.Podi);
 		dosa.spreadFlavour();
 		
 
